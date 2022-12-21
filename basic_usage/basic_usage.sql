e7820a25b32f29c9dedb4ba37aae2391e0fa8740
suwonleee <suwonleee@naver.com>
2022-12-21 19:26:34 +0900

    Add sql string functions

--- basic_usage/basic_usage.sql
+++ basic_usage/basic_usage.sql
@@ -178,5 +178,160 @@ WHERE OrderID LIKE '1025_'
 
 
 -- 숫자로 구성된 문자열은 숫자로 자동인식
+
+
+--9 숫자와 관련된 함수들
+--반올림 / 올림 / 내림
+SELECT 
+  ROUND(0.5),  --반올림
+  CEIL(0.4),  --올림
+  FLOOR(0.6);  --내림
+
+SELECT 
+  Price,
+  ROUND(price),
+  CEIL(price),
+  FLOOR(price)
+FROM Products;
+
+--절대값
+SELECT ABS(1), ABS(-1), ABS(3 - 10); --절대값
+SELECT * FROM OrderDetails
+WHERE ABS(Quantity - 10) < 5;
+
+--최댓값 최솟값
+SELECT 
+  GREATEST(1, 2, 3), --최댓값
+  LEAST(1, 2, 3, 4, 5);  --최솟값
+
+SELECT
+  OrderDetailID, ProductID, Quantity,
+  GREATEST(OrderDetailID, ProductID, Quantity),
+  LEAST(OrderDetailID, ProductID, Quantity)
+FROM OrderDetails;
+
+-- 큰 값, 작은 값, 갯수, 총합, 평균값
+SELECT
+  MAX(Quantity), --가장 큰 값
+  MIN(Quantity), --가장 작은 값
+  COUNT(Quantity), --갯수 null값 제외
+  SUM(Quantity), --총합
+  AVG(Quantity)  --평균 값 
+FROM OrderDetails
+WHERE OrderDetailID BETWEEN 20 AND 30;
+
+--제곱 / 제곱근
+SELECT
+  POW(2, 3),
+  POWER(5, 2),
+  SQRT(16);
+
+-- 소숫점 n자리까지 선택
+SELECT
+  TRUNCATE(1234.5678, 1),
+  TRUNCATE(1234.5678, 2),
+  TRUNCATE(1234.5678, 3),
+  TRUNCATE(1234.5678, -1),
+  TRUNCATE(1234.5678, -2),
+  TRUNCATE(1234.5678, -3);
+
+
+-- 10. 문자열 관련 함수들
+
+-- 대문자로, 소문자로
+SELECT
+  UPPER('abcDEF'),
+  LOWER('abcDEF');
+
+SELECT
+  UCASE(CustomerName),
+  LCASE(ContactName)
+FROM Customers;
+
+-- 괄호 안 내용 이어붙이기
+SELECT CONCAT('HELLO', ' ', 'THIS IS ', 2021)
+SELECT CONCAT_WS('-', 2021, 8, 15, 'AM') --CONCAT_WS는 -로 각 문장을 이어붙이는 것
+SELECT CONCAT('O-ID: ', OrderID) FROM Orders;
+SELECT
+  CONCAT_WS(' ', FirstName, LastName) AS FullName
+FROM Employees;
+
+-- 문자열 자름, 왼쪽부터, 오른쪽부터
+SELECT
+  SUBSTR('ABCDEFG', 3),
+  SUBSTR('ABCDEFG', 3, 2),
+  SUBSTR('ABCDEFG', -4),
+  SUBSTR('ABCDEFG', -4, 2);
+
+SELECT
+  LEFT('ABCDEFG', 3),
+  RIGHT('ABCDEFG', 3);
+
+SELECT
+  OrderDate,
+  LEFT(OrderDate, 4) AS Year,
+  SUBSTR(OrderDate, 6, 2) AS Month,
+  RIGHT(OrderDate, 2) AS Day
+FROM Orders;
+
+--문자열 바이트 길이, 문자 길이
+SELECT
+  LENGTH('ABCDE'),
+  CHAR_LENGTH('ABCDE'),
+  CHARACTER_LENGTH('ABCDE');
+
+SELECT
+  LENGTH('안녕하세요'), -- 15
+  CHAR_LENGTH('안녕하세요'), -- 5
+  CHARACTER_LENGTH('안녕하세요'); -- 5
+
+-- 공백 제거
+SELECT
+  CONCAT('|', ' HELLO ', '|'),
+  CONCAT('|', LTRIM(' HELLO '), '|'),
+  CONCAT('|', RTRIM(' HELLO '), '|'),
+  CONCAT('|', TRIM(' HELLO '), '|');
+
+SELECT * FROM Categories
+WHERE CategoryName = ' Beverages '
+
+SELECT * FROM Categories
+WHERE CategoryName = TRIM(' Beverages ')
+
+-- 자릿수 채워넣기
+SELECT
+  LPAD('ABC', 5, '-'),
+  RPAD('ABC', 5, '-');
+
+SELECT
+  LPAD(SupplierID, 5, 0),
+  RPAD(Price, 6, 0)
+FROM Products;
+
+-- 대체 A 에서 B를 C로 변경
+SELECT
+  REPLACE('맥도날드에서 맥도날드 햄버거를 먹었다.', '맥도날드', '버거킹');
+SELECT
+  REPLACE(Description, ', ', ' and ')
+FROM Categories;
+
+-- STR 안에 문자 확인 , 없을 시 0
+SELECT
+  INSTR('ABCDE', 'ABC'),
+  INSTR('ABCDE', 'BCDE'),
+  INSTR('ABCDE', 'C'),
+  INSTR('ABCDE', 'DE'),
+  INSTR('ABCDE', 'F');
+
+SELECT * FROM Customers
+WHERE INSTR(CustomerName, ' ') BETWEEN 1 AND 6;
+-- < first name이 한글자부터 다섯글자까지 이신 분들
+
+-- A를 B 자료형으로 변환
+SELECT
+  '01' = '1',
+  CONVERT('01', DECIMAL) = CONVERT('1', DECIMAL);
+
 -- *******************************
 -- 각종 연산자들
+
